6bbba2eada38c7278d90fe7572c5146c855adf57
harman kaur <harmankaur2@academic.rrc.ca>
2025-11-06 18:35:17 -0600

    Setup the baisc endpoints.

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ dotenv.config()
 
 //Other imports
 import morgan from "morgan";
+import alertRoutes from "../src/api/v1/routes/alertRoutes"
+import forecastRoutes from "../src/api/v1/routes/forecastroutes"
+import locationRoutes from "../src/api/v1/routes/locationRoutes"
+
 import setupSwagger from "../config/swagger";
 
 //Express app created 
@@ -23,6 +27,14 @@ app.get("/health", (_req: Request, res: Response) => {
   res.status(200).send("Server is healthy");
 });
 
+//Forecast endpoint
+app.use("/api/v1/forecasts", forecastRoutes)
+
+//Locations endpoint
+app.use("/api/v1/locations", locationRoutes)
+
+//alerts endpoint
+app.use("api/v1/alerts", alertRoutes)
 
 
 //Setup swagger
