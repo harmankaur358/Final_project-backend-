ed47a251c803c8e41841fe122405fdfae5b95cc3
harman kaur <harmankaur2@academic.rrc.ca>
2025-11-21 12:32:34 -0600

    Updated the app.ts

--- src/app.ts
+++ src/app.ts
@@ -11,20 +11,33 @@ import alertRoutes from "../src/api/v1/routes/alertRoutes"
 import forecastRoutes from "../src/api/v1/routes/forecastroutes"
 import locationRoutes from "../src/api/v1/routes/locationRoutes"
 import setupSwagger from "../config/swagger";
+import { accessLogger, errorLogger, consoleLogger } from "./api/v1/middleware/logger";
+import errorHandler from "./api/v1/middleware/errorHanlder"
+//import authMiddleware from "./api/v1/middleware/authenticate";
 
 //Express app created 
 const app: Express = express();
 
+// Logging middleware
+if (process.env.NODE_ENV === "production") {
+  app.use(accessLogger);
+  app.use(errorLogger);
+} else {
+  app.use(consoleLogger);
+
+  app.use(morgan("dev"));
+}
+
 // Parsing json request
 app.use(express.json());
-
-// HTTP request logging with Morgan
-app.use(morgan("combined"));
 
 //health check endpoint
 app.get("/health", (_req: Request, res: Response) => {
   res.status(200).send("Server is healthy");
 });
+
+// Apply authentication middleware before routes
+//app.use(authMiddleware);
 
 //Forecast endpoint
 app.use("/api/v1/forecasts", forecastRoutes)
@@ -37,6 +50,9 @@ app.use("/api/v1/alerts", alertRoutes)
 
 //Setup swagger
 setupSwagger(app)
+
+// Global error handler 
+app.use(errorHandler);
 
 //Exporting app
 export default app;
